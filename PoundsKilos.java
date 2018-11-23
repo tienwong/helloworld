3977510605a50f96623eee4bc3c3398cd01cc900
tienwong <45297291+tienwong@users.noreply.github.com>
2018-11-23 15:23:57 -0600

    Add files via upload

---
+++ PoundsKilos.java
@@ -0,0 +1,15 @@
+
+public class PoundsKilos {
+
+	public static void main(String[] args) {
+		// Convert a number of pounds to kilograms.
+		// 1. Create a variable to store the number of pounds.
+		// 2. Calculate the number of kilograms for the # above & store as a variable.
+		// 3. Print out the result.
+		// Note: 1 pound is equal to 0.45359237 kilograms.
+		double numberOfPounds = 50d;
+		double numberOfKilograms = numberOfPounds * 0.45359237d;
+		System.out.println(numberOfPounds + " pounds is equal to " + numberOfKilograms + " kilograms.");
+	}
+
+}
